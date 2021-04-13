c1e1c799683a1f967f45e90e1692d6a0cac7fb34
Truong Le Van Cuong <truonglevancuong92@gmail.com>
2021-04-13 22:44:55 +0700

    Update test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,12 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_test_run/app/screens/dashboard_screen/dashboard_screen.dart';
+import 'package:flutter_test_run/app/screens/home_screen/home_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(DashboardScreen());
+    await tester.pumpWidget(MaterialApp(
+      home: HomeScreen(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
